1fceb47348403ec292184ed1842ab57e882a5088
Mostafa-Mobarak22 <mostafamobarak89@gmail.com>
2024-10-19 14:35:59 +0300

    search

--- real_state/property/api/views.py
+++ real_state/property/api/views.py
@@ -30,3 +30,42 @@ def newest_properties(request):
         property_json = PropertySerializer(property,many=True)
         return Response(data=property_json.data, status=status.HTTP_201_CREATED)
     return Response(status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(["POST"])
+def same_properties_type(request):
+    if request.method == 'POST':
+        property = Property.objects.filter(commercial=request.data["commercial"])
+        property_json = PropertySerializer(property,many=True)
+        return Response(data=property_json.data, status=status.HTTP_201_CREATED)
+    return Response(status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(["POST"])
+def search(request):
+    if request.method == 'POST':
+        properties = Property.objects.filter(is_published=True)
+        if(request.data["buy"]):
+            properties = properties.filter(is_sale=request.data["buy"])
+        if(request.data["bed"]):
+            properties = properties.filter(bed=request.data["bed"])
+        if(request.data["bath"]):
+            properties = properties.filter(bath=request.data["bath"])
+        if(request.data["usage"]):
+            properties = properties.filter(property_type=request.data["usage"])
+        if(request.data["residential"]):
+            properties = properties.filter(commercial=request.data["residential"])
+        elif(request.data["commercial"]):
+            properties = properties.filter(commercial=request.data["commercial"])
+        if(request.data["maxarea"]):
+            properties = properties.filter(area__lte=request.data["maxarea"])
+        elif(request.data["minarea"]):
+            properties = properties.filter(area__gte=request.data["minarea"])
+        if(request.data["maxprice"]):
+            properties = properties.filter(price__lte=request.data["maxprice"])
+        elif(request.data["minprice"]):
+            properties = properties.filter(price__gte=request.data["minprice"])
+        if(request.data["search"]):
+            search = request.data.get('search', '')
+            properties = properties.filter(location__icontains=search)
+        property_json = PropertySerializer(properties, many=True)
+        return Response(data=property_json.data, status=status.HTTP_201_CREATED)
+    return Response(status=status.HTTP_400_BAD_REQUEST)
--- real_state/property/urls.py
+++ real_state/property/urls.py
@@ -1,7 +1,7 @@
 # property/urls.py
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .api.views import PropertyViewSet, PropertyImageViewSet,user,newest_properties
+from .api.views import PropertyViewSet, PropertyImageViewSet,user,newest_properties,same_properties_type,search
 
 router = DefaultRouter()
 router.register(r'properties', PropertyViewSet)
@@ -11,4 +11,6 @@ urlpatterns = [
     path('', include(router.urls)),
     path('user/<int:user_id>/',user),
     path('newest/',newest_properties),
+    path('same/',same_properties_type),
+    path('search/',search),
 ]
